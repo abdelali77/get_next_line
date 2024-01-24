7e7cb14c58f34e449181f7e6739044920dc54663
Abdelali <abde69436@gmail.com>
2024-01-24 18:11:07 +0100

    main

---
+++ get_next_line_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/24 13:18:29 by abmahfou          #+#    #+#             */
+/*   Updated: 2024/01/24 17:53:11 by abmahfou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include "stdlib.h"
+# include "unistd.h"
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+int		found_newline(char *line);
+char	*line_remaining(char *line);
+size_t	ft_strlen(const char *str);
+void	*ft_calloc(size_t count, size_t size);
+
+#endif
