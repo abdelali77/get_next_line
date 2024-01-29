1d4b820727055c52392af9fe2263cca2d24220ed
Abdelali <abde69436@gmail.com>
2024-01-29 15:01:45 +0100

    bonus

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 13:19:10 by abmahfou          #+#    #+#             */
-/*   Updated: 2024/01/25 17:25:00 by abmahfou         ###   ########.fr       */
+/*   Updated: 2024/01/29 12:21:32 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,23 +24,6 @@ size_t	ft_strlen(const char *str)
 	return (len);
 }
 
-void	*ft_calloc(size_t count, size_t size)
-{
-	unsigned char	*calloc_ptr;
-	size_t			i;
-
-	i = 0;
-	calloc_ptr = malloc(count * size);
-	if (calloc_ptr == NULL)
-		return (NULL);
-	while (i < count)
-	{
-		calloc_ptr[i] = '\0';
-		i++;
-	}
-	return (calloc_ptr);
-}
-
 char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*str;
@@ -53,17 +36,19 @@ char	*ft_strjoin(char *s1, char *s2)
 		return (NULL);
 	if (!s1)
 	{
-		s1 = ft_calloc(1, sizeof(char));
+		s1 = malloc(1 * sizeof(char));
 		if (s1 == NULL)
 			return (NULL);
+		s1[0] = '\0';
 	}
-	str = ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1, sizeof(char));
+	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
 	while (s1[++i])
 		str[i] = s1[i];
 	while (s2[j])
 		str[i++] = s2[j++];
+	str[i] = '\0';
 	free(s1);
 	return (str);
 }
@@ -99,18 +84,19 @@ char	*line_remaining(char *line)
 		return (NULL);
 	if (i == 0)
 	{
-		remain = ft_calloc(1, sizeof(char));
+		remain = malloc(1 * sizeof(char));
 		if (remain == NULL)
 			return (NULL);
 	}
 	else
 	{
-		remain = ft_calloc(ft_strlen(line) - i + 1, sizeof(char));
+		remain = malloc((ft_strlen(line) - i + 1) * sizeof(char));
 		if (remain == NULL)
 			return (NULL);
 		while (line[i])
 			remain[j++] = line[i++];
 	}
+	remain[j] = '\0';
 	free(line);
 	return (remain);
 }
